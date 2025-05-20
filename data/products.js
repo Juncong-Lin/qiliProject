f0f551e5f4c175e56cb4d8cd00cf666d5688638a
Jack <20240810@tutamail.com>
2025-05-20 11:14:44 +0800

    Use module and imput products from products.js

--- data/products.js
+++ data/products.js
@@ -1,4 +1,4 @@
-const products = [
+export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     image: 'images/products/Epson-DX5-Printhead-for-Chinese-Printers-Epson-F186000-Universal-New-Version-6578.jpg',
