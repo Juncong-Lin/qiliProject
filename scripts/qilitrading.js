17d843780e9789c51f48e1b41742943142292607
Jack <20240810@tutamail.com>
2025-05-20 09:19:17 +0800

    Make the cart quantity interactive.

--- scripts/qilitrading.js
+++ scripts/qilitrading.js
@@ -45,19 +45,18 @@ products.forEach((product) => {
 
 document.querySelector('.js-prodcts-grid').innerHTML = productsHTML;
 
+
 document.querySelectorAll('.js-add-to-cart')
 .forEach((button) => {
   button.addEventListener('click', () => {
     const productId = button.dataset.productId;
 
     let matchingItem;
-
     cart.forEach((item) => {
       if (productId === item.productId) {
         matchingItem = item;
       }
     });
-
     if (matchingItem) {
         matchingItem.quantity += 1;
       } else { 
@@ -65,7 +64,15 @@ document.querySelectorAll('.js-add-to-cart')
           productId : productId,    
           quantity: 1
         })
-      }  
+      }
+
+    let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+    console.log(cartQuantity)
     console.log(cart)
   });
 });
+
